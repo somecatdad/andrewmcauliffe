2e632f4ab9e12438971780a913630ad0eb135a2d
Andrew McAuliffe <andrew.mcauliffe@gmail.com>
2022-01-29 23:10:13 -0600

    Add content and resume PDF

--- src/App.js
+++ src/App.js
@@ -1,8 +1,20 @@
 import './App.css';
+import resumePdf from './andrew-mcauliffe-resume.pdf';
 
 function App() {
   return (
-    <div className="App" />
+    <div className="App">
+      <div className="Main">
+        <div className="Header">Andrew McAuliffe</div>
+        <div className="Subheader">React • React Native • Team Leadership • Scrum</div>
+        <div className="Content">
+          <a href={resumePdf}>
+            <span>Résumé</span>
+            <span className="TextBadge">PDF</span>
+          </a>
+        </div>
+      </div>
+    </div>
   );
 }
 
